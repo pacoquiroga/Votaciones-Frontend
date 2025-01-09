d35998a7fdbd41f0d0cb0db59129f5d58992854f
darivadeneira <generalmayorxd@gmail.com>
2025-01-09 11:09:41 -0500

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { useState } from "react";
 import CandidatosCard from "./components/CandidatoCard";
-import RecintoSeleccionado from "./components/recinto/RecintoSeleccionado";
 
 function App() {
   return (
     <>
-      <div className="bg-white flex justify-center items-center">	
-        <RecintoSeleccionado />
-      </div>
       <div className="bg-white flex justify-center items-center">
         <CandidatosCard nombre={"Daniel Noboa"} partido={"ADN"} num_lista={"1"} imagen={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYUY7htlIIUKF2SiKc3D3NCOggJlNja8E_hg&s"}/>
       </div>
